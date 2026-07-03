b24fb3e1c15543b4360931ce1c409f59df81c560
Sangita Nath <sangitanath8197@hotmail.com>
2026-07-03 13:37:23 +0530

    Add service worker for caching and offline support

---
+++ public/sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'learntrack-cache';
+const APP_SHELL = ['/learntrack/', '/learntrack/index.html', '/learntrack/manifest.webmanifest', '/learntrack/icon.svg'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) => Promise.all(names.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request)
+        .then((response) => {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+          return response;
+        })
+        .catch(() => caches.match('/learntrack/index.html'));
+    })
+  );
+});
